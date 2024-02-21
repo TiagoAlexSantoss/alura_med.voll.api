1199eb64c1b8365f1330981925d1fa85c8f30d65
TIAGO <tiagoalexdossantos@gmail.com>
2024-02-21 00:13:38 -0300

    commit 02

--- src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
+++ src/main/java/med/voll/api/domain/consulta/DadosDetalhamentoConsulta.java
@@ -1,7 +1,11 @@
 package med.voll.api.domain.consulta;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 
+@Service
 public record DadosDetalhamentoConsulta(Long id, Long idMedico, Long idPaciente, LocalDateTime data) {
     public DadosDetalhamentoConsulta(Consulta consulta) {
         this(consulta.getId(), consulta.getMedico().getId(), consulta.getPaciente().getId(), consulta.getData());
--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -14,7 +14,7 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
     @Query("""
             select m from Medico m
             where
-            m.ativo = 1
+            m.ativo = true
             and
             m.especialidade = :especialidade
             and
@@ -27,6 +27,7 @@ public interface MedicoRepository extends JpaRepository<Medico, Long> {
             limit 1
         """)
     Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
+
 
     @Query("""
             select m.ativo
